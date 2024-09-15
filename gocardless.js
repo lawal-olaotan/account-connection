8f4fdb1aa7c35ffbe92ee975794f41c15bcf7eec
Olaotan Lawal <olaotan104@gmail.com>
2024-09-15 17:26:27 +0100

    Commit introduced the following: - Refined gocardless apis - Introduced Novu workflow - Introduced whatsapp reminder module

--- gocardless.js
+++ gocardless.js
@@ -1,9 +1,9 @@
 /**
  * @file Defines go cardless client.
  */
- 
-import NordigenClient from "nordigen-node";
+ import NordigenClient from "nordigen-node";
 import dotenv from "dotenv"
+import { refreshToken} from "./db/queries/token.js";
 
 
 dotenv.config();
@@ -13,6 +13,31 @@ export const cardlessClient = new NordigenClient({
     secretId: process.env.SECRET_ID,
     secretKey: process.env.SECRET_KEY
 });
+
+/**
+ * function calls open banking 
+ * @param {*} route 
+ * @param {*} method 
+ * @param {*} data 
+ */
+export const openBankingApi  = async( route, method, data) => {
+    const client = await refreshToken(); 
+    const uri = process.env.GOCARDLESS_URI
+
+    const parameter = {
+        method,
+        headers:{
+            "accept": "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${client}`
+        },
+        body: JSON.stringify(data)
+    }
+
+    const response = await fetch(`${uri}${route}`,parameter)
+    const responseJson = await response.json()
+    return responseJson
+}
 
 
 
